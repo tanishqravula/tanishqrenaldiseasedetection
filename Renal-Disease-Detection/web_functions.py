e51709af9113270350c7dc3469645fdec5945d69
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-05-07 01:28:02 +0530

    Update web_functions.py

--- Renal-Disease-Detection/web_functions.py
+++ Renal-Disease-Detection/web_functions.py
@@ -12,7 +12,7 @@ def load_data():
     """This function returns the preprocessed data"""
 
     # Load the Diabetes dataset into DataFrame.
-    df = pd.read_csv('kidney.csv')
+    df = pd.read_csv('Renal-Disease-Detection/kidney.csv')
 
     # Rename the column names in the DataFrame.
     
